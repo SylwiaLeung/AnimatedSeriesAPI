e27e744ea2942a5f49aca06d229d9aadeed34c58
SylwiaLeung <longdepati@gmail.com>
2021-12-01 11:31:31 +0100

    Implement BadRequestException and add ExceptionHandlingMiddleware

---
+++ AnimatedSeriesAPI/Exceptions/BadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimatedSeriesAPI.Middleware
+{
+    [Serializable]
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+    }
+}
