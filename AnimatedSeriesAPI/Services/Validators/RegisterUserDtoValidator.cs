03eabd3bf0282789d318b806c56157e94f006391
SylwiaLeung <longdepati@gmail.com>
2021-12-02 13:05:56 +0100

    Create external class for managing resource service'

--- AnimatedSeriesAPI/Services/Validators/RegisterUserDtoValidator.cs
+++ AnimatedSeriesAPI/Services/Validators/RegisterUserDtoValidator.cs
@@ -6,24 +6,25 @@ namespace AnimatedSeriesAPI.Models
 {
     public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
     {
+
         [System.Obsolete]
-        public RegisterUserDtoValidator(SeriesDbContext dbContext)
+        public RegisterUserDtoValidator(SeriesDbContext dbContext, ResourceManagerService resourceManager)
         {
             CascadeMode = CascadeMode.StopOnFirstFailure;
 
             RuleFor(x => x.Email).NotEmpty().EmailAddress();
             RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password cannot be empty")
-                .MinimumLength(6).WithMessage("Password must contain at least 6 caracters")
-                .Matches("[A-Z]").WithMessage("Password must contain an upper-case letter")
-                .Matches("[a-z]").WithMessage("Password must contain a lower-case letter")
-                .Matches("[0-9]").WithMessage("Password must contain at least 1 digit");
+                .NotEmpty().WithMessage(resourceManager.Manager.GetString("passwordEmpty"))
+                .MinimumLength(6).WithMessage(resourceManager.Manager.GetString("passwordMinCharacters"))
+                .Matches("[A-Z]").WithMessage(resourceManager.Manager.GetString("passwordUpperCase"))
+                .Matches("[a-z]").WithMessage(resourceManager.Manager.GetString("passwordLowerCase"))
+                .Matches("[0-9]").WithMessage(resourceManager.Manager.GetString("passwordMinDigit"));
             RuleFor(x => x)
                 .Custom((value, context) =>
                 {
                     if (value.Password != value.ConfirmPassword)
                     {
-                        context.AddFailure(nameof(value.Password), "Passwords should match");
+                        context.AddFailure(nameof(value.Password), resourceManager.Manager.GetString("passwordMatch"));
                     }
                 });
 
@@ -33,7 +34,7 @@ namespace AnimatedSeriesAPI.Models
                     var emailUsed = dbContext.Users.Any(u => u.Email == value);
                     if (emailUsed)
                     {
-                        context.AddFailure("Email", "This email address is already registered");
+                        context.AddFailure("Email", resourceManager.Manager.GetString("emailTaken"));
                     }
                 });
         }
