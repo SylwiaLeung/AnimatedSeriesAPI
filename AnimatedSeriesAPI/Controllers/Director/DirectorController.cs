07d2e8901ec17c66c7f1d601a3b279a46a74d2bb
Karol Bieniaszewski <karol.bieniaszewski.30@gmail.com>
2021-12-02 14:16:52 +0100

    Add director swagger documentation

--- AnimatedSeriesAPI/Controllers/Director/DirectorController.cs
+++ AnimatedSeriesAPI/Controllers/Director/DirectorController.cs
@@ -15,7 +15,7 @@ namespace AnimatedSeriesAPI.Controllers.Director
     /// <summary>
     /// Director API controller offers GET, POST, PATCH, DELETE request methods
     /// </summary>
-    [Route("api/[controller]")]
+    [Route("api/directors")]
     [ApiController]
     public class DirectorController : ControllerBase
     {
@@ -31,31 +31,33 @@ namespace AnimatedSeriesAPI.Controllers.Director
         /// <summary>
         /// GET method returns all directors
         /// </summary>
-        /// <returns>Return list of DirectorShortDtos and 200 OK</returns>
+        /// <returns>Returns list of DirectorShortDtos</returns>
+        /// <response code="200">Returns dtos for all directors in databse</response>
         [HttpGet]
-        //[Route("directors")]
         public async Task<ActionResult<IEnumerable<DirectorShortDto>>> GetAllDirectors()
         {
             return Ok(await _directorRepo.GetAll());
         }
-        
+
         /// <summary>
         /// GET method return director specified by id
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns>Return specified DirectorLongDto and 200 Ok</returns>
+        /// <param name="directorId"></param>
+        /// <returns>Returns specified DirectorLongDto</returns>
+        /// <response code="200">Returns specifed director's dto</response>
         [HttpGet]
-        [Route("{id}")]
-        public async Task<ActionResult<DirectorLongDto>> GetDirector(int id)
+        [Route("{directorId}")]
+        public async Task<ActionResult<DirectorLongDto>> GetDirector(int directorId)
         {
-            return Ok(await _directorRepo.GetSingle(id));
+            return Ok(await _directorRepo.GetSingle(directorId));
         }
 
         /// <summary>
-        /// GET method return season list of specifed director
+        /// GET method return season list of specifed director's seasons
         /// </summary>
         /// <param name="directorId"></param>
-        /// <returns>Return list of SeasonShortDto and 200 Ok</returns>
+        /// <returns>Return list of SeasonShortDto</returns>
+        /// <response code="200">Returns list of specifed director's seasons dtos</response>
         [HttpGet]
         [Route("{directorId}/seasons")]
         public async Task<ActionResult<IEnumerable<SeasonShortDto>>> GetDirectorAllSeasons(int directorId)
@@ -67,9 +69,18 @@ namespace AnimatedSeriesAPI.Controllers.Director
         /// POST method add new Director to database
         /// </summary>
         /// <param name="directorCreateDto"></param>
-        /// <returns>Return endpoint to new object and 201 Created</returns>
+        /// <returns>Return endpoint to new object</returns>
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "name": "New Director"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns endpoint to new director</response>
         [HttpPost]
-        //[Route("directors")]
         public async Task<ActionResult> CreateDirector(DirectorCreateDto directorCreateDto)
         {
             int newDirectorId = await _directorRepo.Add(directorCreateDto);
@@ -77,25 +88,44 @@ namespace AnimatedSeriesAPI.Controllers.Director
         }
 
         /// <summary>
-        /// DELETE method 
+        /// DELETE method delete specifed director from database
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns> 204 NoContent</returns>
+        /// <param name="directorId"></param>
+        /// <returns>Return 204 NoContent</returns>
+        /// <response code="204">Returns no content</response>
         [HttpDelete]
-        [Route("{id}")]
-        public async Task<ActionResult> DeleteDirector(int id)
+        [Route("{directorId}")]
+        public async Task<ActionResult> DeleteDirector(int directorId)
         {
-            await _directorRepo.Delete(id);
+            await _directorRepo.Delete(directorId);
             return NoContent();
         }
 
 
-
+        /// <summary>
+        /// PATCH method partial update of specifed director
+        /// </summary>
+        /// <param name="directorId"></param>
+        /// <param name="patchDoc"></param>
+        /// <returns>Returns 204 NoContent</returns>
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     PATCH /Todo
+        ///     [
+        ///       {
+        ///         "op":"replace",
+        ///         "path":"/Name",
+        ///         "value": "Endrju Wajda Test"
+        ///       }
+        ///     ]
+        /// </remarks>
+        /// <response code="204">Returns no content</response>
         [HttpPatch]
-        [Route("{id}")]
-        public async Task<ActionResult> UpdateDirector(int id, JsonPatchDocument<DirectorUpdateDto> patchDoc )
+        [Route("{directorId}")]
+        public async Task<ActionResult> UpdateDirector(int directorId, JsonPatchDocument<DirectorUpdateDto> patchDoc )
         {
-            var directorToUpdate = await _directorRepo.GetById(id);
+            var directorToUpdate = await _directorRepo.GetById(directorId);
 
             DirectorUpdateDto directorToPatch = _mapper.Map<DirectorUpdateDto>(directorToUpdate); 
 
